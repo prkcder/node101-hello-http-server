4e80a1764ecf7d5243f565849d3024ad480d8768
John Park <jhnprk12@gmail.com>
2019-05-12 11:17:19 -0700

    tests are passing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code here
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Hello World");
+    res.end();
+}).listen(8080);
+
+console.log("Server is listening at http://localhost:8080/");
